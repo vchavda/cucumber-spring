345fdc862bf4d3464ab841f5033b30722054c971
Vijaykumar Chavda <vijay.chavda-cic-uk@uk.ibm.com>
2019-11-27 21:53:37 +0000

    added base class

---
+++ src/test/java/cukes/steps/Base.java
@@ -0,0 +1,8 @@
+package cukes.steps;
+
+public class Base {
+    // you can extend your step definition class using this
+    // note you cannot extend step definition classes ie stepdefinitonA class cannot extend StepdefintionB class
+    // but you can extend any other Java class like this one where you can use it for maybe sharing static objects
+
+}
--- src/test/java/cukes/steps/WhenSteps.java
+++ src/test/java/cukes/steps/WhenSteps.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
-public class WhenSteps {
+public class WhenSteps extends Base{
 
     @Autowired
     private ResponseApi respApi;
